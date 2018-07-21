90445eb410e79dbee6557cc398c755e1a2814894
Arnold Bradshaw <ab17afr@herts.ac.uk>
2018-07-21 13:30:55 -0400

    New Upload
    Upload New files for homepage, header, footer, images etc...

--- README.md
+++
@@ -1,7 +0,0 @@
-# ScaffoldTraining
-// Arnold Bradshaw
-// CTS College - Montrose Chaguanas
-// University of Hertfordshire
-
-Scaffold Training Application written in PHP
-
---
+++ scripts/dbase_connect.php
@@ -0,0 +1,10 @@
+<?php
+
+    $con=mysqli_connect("localhost","root","root","ScaffoldTraining");
+
+    //Check connection
+         if(mysqli_connect_errno()){
+             echo "Failed to connect to the database";
+         }
+
+?>
