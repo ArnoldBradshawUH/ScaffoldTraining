f030fe123636006908905b87edf1de6fa94ca328
Arnold Bradshaw <ar17afr@herts.ac.uk>
2018-12-29 20:15:24 -0400

    Update - 2018-12-29
    Update and inclusion of privacy policy for users.

---
+++ privacy.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: said
+ * Date: 2018-12-29
+ * Time: 20:07
+ */
+include('header.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="STC Company" content="We are about Scaffold Training">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Privacy Policy</title>
+    <link rel="stylesheet" href="css/main-style.css">
+</head>
+<body>
+<div class="main-page">
+    <div class="content-wrap">
+        <br>
+        <h2>Privacy Policy</h2><br>
+        <p>
+            The University of Hertfordshire will hold the information you have provided securely in accordance with the
+            Data Protection Act 1998. The University provides regular updates to its enquirers, and may contact you with
+            further information via post or email. Enquirers may also occasionally be asked to take part in Market
+            Research questionnaires. You may choose to unsubscribe from these communications at any time by clicking on the
+            'unsubscribe' link which will appear in the emails you receive.
+        </p><br>
+        <p>
+            Please note that all information provided is treated in the strictest of confidence and will not be used for
+            any other purpose than that intended without your expressed consent.
+        </p>
+    </div>
+</div>
+</body>
+<footer>
+    <?php include('footer.php'); ?>
+</footer>
+</html>
